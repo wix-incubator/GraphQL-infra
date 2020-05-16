166a7644e826a8c3ba3a98680eff11e7474872df
Omer Winter <omerwin@wix.com>
2020-05-16 15:42:21 +0300

    project init

---
+++ graphql-infra/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={'https://upload.wikimedia.org/wikipedia/commons/1/17/GraphQL_Logo.svg'} className="App-logo" alt="logo" />
+          <h2>Welcome to GraphQL</h2>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
